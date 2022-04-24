7332dad20b4297e1298b6926d13d40740d88eee9
join3r <join3r@gmail.com>
2022-04-24 07:19:25 +0200

    update

---
+++ src/iptables.rs
@@ -0,0 +1,6 @@
+use crate::Result;
+use std::net::SocketAddr;
+
+pub fn iptables_add(remote_addr: SocketAddr) -> Result<()> {
+    todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod iptables;
+
 use std::net::SocketAddr;
 
 use rocket::routes;
@@ -9,12 +11,19 @@ type Result<T, E = ErrType> = std::result::Result<T, E>;
 
 #[get("/")]
 async fn auth(remote_addr: SocketAddr) -> String {
+    iptables::iptables_add(remote_addr);
     format!("IP: {remote_addr}")
+}
+
+#[get("/health_check")]
+async fn health_check() -> rocket::http::Status {
+    rocket::http::Status::Ok
 }
 
 #[rocket::main]
 async fn main() -> Result<()> {
     rocket::build()
+        .mount("/", routes![health_check])
         .mount("/v1/auth", routes![auth])
         .launch()
         .await?;
