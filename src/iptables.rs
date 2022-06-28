e417e5f2eb44d404aa3130c2ce3a9138daeafe67
Vladimir 'join3r' Schmotzer <vladimir.schmotzer@dxc.com>
2022-06-28 19:51:49 +0200

    update

--- src/iptables.rs
+++ src/iptables.rs
@@ -1,6 +1,94 @@
-use crate::Result;
-use std::net::SocketAddr;
+use crate::{anyhow, Result};
+use std::{net::SocketAddr, process::Command, str::FromStr, ops::Deref, sync::RwLock};
 
-pub fn iptables_add(remote_addr: SocketAddr) -> Result<()> {
-    todo!()
+const IPTABLES: &str = "/usr/sbin/iptables";
+const CHAIN_NAME: &str = "rprotectgate";
+
+#[derive(Clone)]
+pub struct AllowedList {
+    inner: Vec<Account>,
+}
+
+#[derive(Clone)]
+struct Account {
+    ip: SocketAddr,
+    username: Username,
+}
+
+#[derive(Clone)]
+struct Username {
+    inner: String,
+}
+
+impl AllowedList {
+    pub fn new() -> Result<AllowedList> {
+        let allowed_list = AllowedList {
+            inner: Vec::new(),
+        };
+        
+        // add table ip filter
+        // add chain ip filter rprotectgate { type filter hook input priority 0; policy drop; }
+        // add rule ip filter rprotectgate ip saddr 192.168.100.1 counter accept
+
+        let _res = execute([IPTABLES, "-F", CHAIN_NAME].as_ref())?; // TODO: only if CHAIN_NAME exists
+        let _res = execute([ IPTABLES,"-X", CHAIN_NAME ].as_ref())?;
+        let _res = execute([ IPTABLES, "-N", CHAIN_NAME ].as_ref())?;
+        allowed_list.update()?;
+        let _res = execute([IPTABLES, "-A", CHAIN_NAME, "-j", "DROP"].as_ref())?;
+        Ok(allowed_list)
+    }
+    pub fn update(&self) -> Result<()> {
+        if let Some(ips) = self.get_ips() {
+            let _res = execute([IPTABLES, "-I", CHAIN_NAME, "-s", &ips, "-j", "ACCEPT"].as_ref())?;
+        }
+        Ok(())
+    }
+    pub fn add(&mut self, ip: SocketAddr, username: String) -> Result<()> {
+        self.inner.push(Account {
+            ip, username: username.parse()?
+        });
+        Ok(())
+    }
+    pub fn get_ips(&self) -> Option<String> {
+        if !self.inner.is_empty() {
+            let p = self.inner.iter().map(|acc| acc.username.deref().clone()).collect::<Vec<_>>().join(",");
+            Some(p)
+        } else {
+            None
+        }
+    }
+}
+
+impl FromStr for Username {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self> {
+        match s.len() {
+            3..=12 => Ok(Username { inner: s.to_string() }),
+            _ => Err(anyhow!("Username must be between 3 and 12 characters long"))
+        }
+    }
+}
+
+impl Deref for Username {
+    type Target = String;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+/// execute command
+fn execute(args: &[&str]) -> Result<String> {
+    let command = args[0];
+    let args = args[1..].iter();
+    let output = Command::new(command).args(args).output()?;
+    match output.status.success() {
+        true => Ok(String::from_utf8(output.stdout)?),
+        false => {
+            let stdout =
+                String::from_utf8(output.stdout).unwrap_or("invalid UTF8 in stdout".to_string());
+            let stderr =
+                String::from_utf8(output.stderr).unwrap_or("invalid UTF8 in stderr".to_string());
+            Err(anyhow!("{}\n{}", stdout, stderr))
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,54 @@
 mod iptables;
 
-use std::net::SocketAddr;
+use std::{sync::{Arc, RwLock}, net::ToSocketAddrs};
 
-use rocket::routes;
+use actix_web::{HttpServer, App, get, Responder, HttpResponse, dev::ConnectionInfo, web::Data};
+use iptables::AllowedList;
+use anyhow::{Result, anyhow};
 
-#[macro_use] extern crate rocket;
 
-type ErrType = Box<dyn std::error::Error + Send + Sync + 'static>;
-type Result<T, E = ErrType> = std::result::Result<T, E>;
 
-#[get("/")]
-async fn auth(remote_addr: SocketAddr) -> String {
-    iptables::iptables_add(remote_addr);
-    format!("IP: {remote_addr}")
-}
+// #[get("/")]
+// async fn auth(remote_addr: SocketAddr) -> String {
+    
+//     format!("IP: {remote_addr}")
+// }
 
 #[get("/health_check")]
-async fn health_check() -> rocket::http::Status {
-    rocket::http::Status::Ok
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
 
-#[rocket::main]
-async fn main() -> Result<()> {
-    rocket::build()
-        .mount("/", routes![health_check])
-        .mount("/v1/auth", routes![auth])
-        .launch()
-        .await?;
-    Ok(())
+#[get("/ip_info")]
+async fn ip_info(conn: ConnectionInfo) -> impl Responder {
+    let real_ip = conn.realip_remote_addr().unwrap();
+    let peer_ip = conn.peer_addr().unwrap();
+    dbg!(real_ip);
+    dbg!(peer_ip);
+    let body = format!("real_ip: {real_ip}\npeer_ip: {peer_ip}");
+    HttpResponse::Ok().body(body)
+}
+
+#[get("/")]
+async fn login(conn: ConnectionInfo, allowed: Data<RwLock<AllowedList>>) -> impl Responder {
+    let peer_ip = conn.peer_addr().unwrap().to_socket_addrs().unwrap().next().unwrap();
+    let mut allowed = allowed.write().unwrap();
+    allowed.add(peer_ip, "join3r".into()).unwrap();
+    let body = allowed.get_ips().unwrap();
+    HttpResponse::Ok().body(body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            // .app_data(Data::new(allowed))
+            .service(health_check)
+            .service(ip_info)
+            .app_data(Data::new(RwLock::new(AllowedList::new().unwrap())))
+            .service(login)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
